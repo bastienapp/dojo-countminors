d75d48367eabee190eae2dfc0d275bf33959f23d
bastienwcs <bastien@wildcodeschool.fr>
2018-03-08 09:06:20 +0100

    modif

--- CountMinors.java
+++ CountMinors.java
@@ -1,3 +1,5 @@
 class CountMinors {
 
+    // TODO 
+    
 }
--- solution/CountMinorsTest.java
+++ solution/CountMinorsTest.java
@@ -6,19 +6,22 @@ public class CountMinorsTest extends TestCase {
 	public void testTrue() throws Exception {
 		assertEquals(true, true);
 	}
+
 	@Test
 	public void test1() throws Exception {
 		int[] ages = new int[]{12};
 		assertEquals(1, CountMinors.minors(ages));
 	}
+
 	@Test
 	public void test2() throws Exception {
 		int[] ages = new int[]{23};
 		assertEquals(0, CountMinors.minors(ages));
 	}
+
 	@Test
 	public void test3() throws Exception {
-		int[] ages = new int[]{5,43,8,13,18,98,16};
+		int[] ages = new int[]{5, 43, 8, 13, 18, 98, 16};
 		assertEquals(4, CountMinors.minors(ages));
 	}
 }
