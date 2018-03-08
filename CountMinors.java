f695235a06b2a61fa547dfc1e777a5153d659eaf
bastienwcs <bastien@wildcodeschool.fr>
2018-03-08 10:23:35 +0100

    modif

--- CountMinors.java
+++ CountMinors.java
@@ -1,35 +1,5 @@
 class CountMinors {
 
-    // Compter le nombre de mineurs dans un tableau.
-    public static int countWhile(int[] tbMinors)
-    {   
-        int i = 0;
-        int minor = 0;
-        while (i < tbMinors.length) 
-        {
-            int value = tbMinors[i];
-            if (value < 18)
-            {
-                minor = minor + 1;
-            }
-            i = i + 1;
-        }
-            
-        return minor;
-    }
-
-    public static int countFor(int[] tbMinors)
-    {   
-        int minor = 0;
-        for (int i = 0; i < tbMinors.length; i = i + 1) 
-        {
-            int value = tbMinors[i];
-            if (value < 18)
-            { 
-                minor = minor + 1;
-            }
-        }
-            
-        return minor;
-    }
+    // TODO
+    
 }
--- CountMinorsTest.java
+++ CountMinorsTest.java
@@ -7,15 +7,6 @@ public class CountMinorsTest extends TestCase {
 		assertEquals(true, true);
 	}
 
-	@Test
-	public void testMinor1() throws Exception {
-        int[] tabTest = new int[]{1, 28, 12, 50, 13};
-		assertEquals(3, CountMinors.countWhile(tabTest));
-	}
-
-	@Test
-	public void testMinorEmpty() throws Exception {
-        int[] tabTest = new int[]{};
-		assertEquals(0, CountMinors.countWhile(tabTest));
-	}
+	// TODO
+    
 }
